46eed5512ca1961f8fadea2cbc4731c608f40625
shaankhosla <skhoslajr@gmail.com>
2018-07-22 02:09:07 -0400

    regex

--- tempscrape.py
+++ tempscrape.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-
+import re
 
 url = 'https://genius.com/Eminem-river-lyrics'
 
@@ -11,3 +11,5 @@ source = source.text
 
 new = source.split('<div class="lyrics">')[1]
 new = new.split('<!--/sse-->')[0]
+
+clean = re.sub('<[^>]+>', '', new)
