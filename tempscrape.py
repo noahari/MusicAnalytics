4c5872456bb7dcc19eb6ce8cef5857d1cee0a4db
shaankhosla <skhoslajr@gmail.com>
2018-07-22 02:53:10 -0400

    function

--- tempscrape.py
+++ tempscrape.py
@@ -4,12 +4,31 @@ from bs4 import BeautifulSoup
 import numpy as np
 import re
 
+
+
+
+def obtain_lyrics():
+    source = requests.get(url).text
+    source = source.split('<div class="lyrics">')[1]
+    source = source.split('<!--/sse-->')[0]
+    clean = re.sub('<[^>]+>', '', source).strip()
+
+
+
+def sentiment_analysis():
+
+
+
+
+
+
+
+
+
+
+
+
 url = 'https://genius.com/Eminem-river-lyrics'
 
-source = requests.get(url)
-source = source.text
 
-new = source.split('<div class="lyrics">')[1]
-new = new.split('<!--/sse-->')[0]
 
-clean = re.sub('<[^>]+>', '', new)
