e2cba0a2ed5ec1455b9f9d9a7647cec3e9cb6c5f
shaankhosla <skhoslajr@gmail.com>
2018-07-22 13:17:48 -0400

    minor changes

--- tempscrape.py
+++ tempscrape.py
@@ -8,9 +8,12 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
 
 
-def obtain_lyrics(url):
+def scrape_lyrics(title, artist):
     ##Takes the Url and strips the html down to just the lyrics
     ##The Lyrics are placed into var clean
+    title = title.replace(" ", "-")
+    artist = artist.replace(" ", "-")
+    url = url = 'https://genius.com/'+artist+'-'+title+'-lyrics'
     source = requests.get(url).text
     source = source.split('<div class="lyrics">')[1]
     source = source.split('<!--/sse-->')[0]
@@ -40,18 +43,23 @@ def scansion_scanner(lyrics):
 def reading_level(lyrics):
     rl = textstat.flesch_reading_ease(lyrics)
     return rl
-
+#100.00-90.00 	5th grade 	Very easy to read. Easily understood by an average 11-year-old student.
+#90.0–80.0 	6th grade 	Easy to read. Conversational English for consumers.
+#80.0–70.0 	7th grade 	Fairly easy to read.
+#70.0–60.0 	8th & 9th grade 	Plain English. Easily understood by 13- to 15-year-old students.
+#60.0–50.0 	10th to 12th grade 	Fairly difficult to read.
+#50.0–30.0 	College 	Difficult to read.
+#30.0–0.0 	College graduate 	Very difficult to read. Best understood by university graduates. 
 
 
 ##InputGrabbers, commented out til GUI figured out
     ##title=Raw_Input('Song Title?')
     ##artist=Raw_Input('Artist name?')
-##The Url for the song input
 title = 'look-at-me'
 artist = 'xxxtentacion'
-url = 'https://genius.com/'+artist+'-'+title+'-lyrics'
 
-lyrics = obtain_lyrics(url)
+
+lyrics = scrape_lyrics(title, artist)
 sentiment = sentiment_analysis(lyrics)
 syllables = scansion_scanner(lyrics)
 
