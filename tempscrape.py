c93d8fd97803c9ad3d3acca35eb698b2c6bf5864
noahari <noahari@gmail.com>
2018-07-22 02:51:00 -0400

    url process tweak for input

--- tempscrape.py
+++ tempscrape.py
@@ -3,12 +3,17 @@ import requests
 import numpy as np
 import re
 
-url = 'https://genius.com/Eminem-river-lyrics'
+##The Url for the song input
+songtitle = 'River'
+artist - 'Eminem'
+url = 'https://genius.com/'+artist+'-'+songtitle+'-lyrics'
 
+##Takes the Url and strips the html down to just the lyrics
+##The Lyrics are placed into var clean
 source = requests.get(url)
 source = source.text
 
 new = source.split('<div class="lyrics">')[1]
 new = new.split('<!--/sse-->')[0]
 
-clean = re.sub('<[^>]+>', '', source).strip()
+clean = re.sub('<[^>]+>', '', new).strip()
