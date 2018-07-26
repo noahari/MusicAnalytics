de6ca08a23566e04fa44699b3c0876603488a001
shaankhosla <skhoslajr@gmail.com>
2018-07-25 22:23:04 -0400

    tried to jsonify

--- app.py
+++ app.py
@@ -9,18 +9,19 @@ import json
 import data_scrape
 from flask import Flask, render_template
 import pandas as pd
-
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    song_list = data_scrape.search_album_id('wolf', 'tyler the creator')
+    song_list = data_scrape.search_song_id('zipper', 'brockhampton')
     df = data_scrape.assemble_df(song_list)
     df = df.drop(['track', 'lyrics', 'id', 'word_frequency'], 1)
  
+    
     chart_data = df.to_dict(orient='records')
     data = json.dumps(chart_data)
-    #data = {'chart_data': chart_data}
+    data = json.loads(data)   
     return render_template("index.html", data = data)
 
 
