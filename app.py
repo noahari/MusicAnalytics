07a876f942cf060bd367adc2b8591b444dfedf1c
shaankhosla <skhoslajr@gmail.com>
2018-07-29 20:31:21 -0400

    fixed form

--- app.py
+++ app.py
@@ -15,10 +15,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
 def index():
+    
     if request.method == 'POST':  #this block is only entered when the form is submitted
         album = request.form.get('album')
         artist = request.form['artist']
-        
+
         df = data_scrape.search_album_id(album, artist)
         df = data_scrape.assemble_df(df)
 
@@ -31,11 +32,8 @@ def index():
         return render_template("index.html", data = data)
         
 
-    return '''<form method="POST">
-                  album: <input type="text" name="album"><br>
-                  artist: <input type="text" name="artist"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
+    return render_template("index.html", data = [{'Enter a song':1}])
+        
 
 
 
@@ -43,3 +41,7 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
