8599fc14cef69695c4a0906b566903f1801716a7
noahari <noahari@gmail.com>
2018-07-30 18:58:12 -0400

    broken html flask bugs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST']) #allow both GET and POST requests
 def index():
     
-    if request.method == 'POST':  #this block is only entered when the form is submitted
+    if request.form["submit"] == "New Album!":  #this block is only entered when the form is submitted
         album = request.form.get('album')
         artist = request.form['artist']
 
@@ -31,7 +31,22 @@ def index():
         data = json.loads(data)
         return render_template("index.html", data = data)
         
+    
+    if request.form["button"] == "Add Tracks!":
+        album = request.form.get('album')
+        artist = request.form['artist']
 
+        df = data_scrape.search_album_id(album, artist)
+        df = data_scrape.assemble_df(df)
+
+        clean = df.drop(['artist', 'id', 'lyrics', 'Word Frequency'], 1)
+        clean = clean.astype(str)
+
+        chart_data = clean.to_dict(orient='records')
+        data = json.dumps(chart_data)
+        data = json.loads(data)
+        return render_template("index.html", data = data)
+    
     return render_template("index.html", data = [{'Enter a song':1}])
 
 
